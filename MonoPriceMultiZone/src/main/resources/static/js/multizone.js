09542caa2706a67f79137d018f3b636531ba8d74
Fabio Garcia <fabio.garcia20@gmail.com>
2014-12-09 20:07:11 -0500

    Added Panel Snap

---
+++ MonoPriceMultiZone/src/main/resources/static/js/multizone.js
@@ -0,0 +1,54 @@
+$(function() {
+	    // setup master volume
+	    $(".master").slider({
+			value : 60,
+			orientation : "horizontal",
+			range : "min",
+			animate : true
+	    });
+	    // setup graphic EQ
+	    $(".level").each(function() {
+			// read initial values from markup and remove that
+			var value = parseInt($(this).text(), 10);
+			$(this).empty().slider({
+			    value : value,
+			    range : "min",
+			    max : 100,
+			    animate : true,
+			    orientation : "vertical"
+			});
+	    });
+	    $('.pwrbtn').bind('click', function() {
+			$(this).toggleClass('active');
+	    });
+	    $( window ).resize(function() {
+		  if($( window ).width() < 768){
+		      setupMobile();
+		  }else{
+		      setupDesktop();
+		  }
+		});
+	    var viewwidth = $(window).width(); 
+	    	
+		if(viewwidth < 768){
+		    setupMobile();
+		    
+		 }
+		
+		    
+		  
+		// scripts
+	function setupMobile(){
+	    if($('section').hasClass('col-md-4')){
+		$('section').removeClass('col-md-4');
+		$('.section-wrapper').panelSnap();
+		
+	    }
+	}
+	function setupDesktop(){
+	    if(!$('section').hasClass('col-md-4')){
+		$('section').addClass('col-md-4');
+		$('.section-wrapper').panelSnap('destroy');
+	    }
+	}
+	});
